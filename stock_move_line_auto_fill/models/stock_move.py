0d1dabb74cbaaae5f6ac18be7a7c50edac2d932e
Rinaldi Firdaus <rinaldi@quartile.co>
2023-11-30 03:09:40 +0000

    [3891][IMP] stock_move_line_auto_fill: reflect oca updates

--- stock_move_line_auto_fill/models/stock_move.py
+++ stock_move_line_auto_fill/models/stock_move.py
@@ -15,24 +15,21 @@ class StockMove(models.Model):
     def _get_avoid_lot_assignment_flag(self):
         return self.picking_id.picking_type_id.avoid_lot_assignment
 
-    def _check_auto_fill_conditions(self, move_line_vals):
-        auto_fill = self._get_auto_fill_flag()
-        avoid_lot_assignment = self._get_avoid_lot_assignment_flag()
-        lot_id = move_line_vals.get("lot_id")
-        return auto_fill and not (avoid_lot_assignment and lot_id)
-
-    def _update_qty_done(self, move_line_vals):
-        if self._check_auto_fill_conditions(move_line_vals):
-            move_line_vals["qty_done"] = move_line_vals.get("reserved_uom_qty", 0.0)
-
     def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
         """
         Auto-assign as done the quantity proposed for the lots.
         Keep this method to avoid extra write after picking _action_assign
         """
-        move_line_vals = super()._prepare_move_line_vals(quantity, reserved_quant)
-        self._update_qty_done(move_line_vals)
-        return move_line_vals
+        self.ensure_one()
+        res = super()._prepare_move_line_vals(quantity, reserved_quant)
+        if not self._get_auto_fill_flag():
+            return res
+        if self._get_avoid_lot_assignment_flag() and res.get("lot_id"):
+            return res
+        if self.quantity_done != self.product_uom_qty:
+            # Not assign qty_done for extra moves in over processed quantities
+            res.update({"qty_done": res.get("reserved_uom_qty", 0.0)})
+        return res
 
     def _action_assign(self, force_qty=False):
         """
@@ -42,16 +39,16 @@ class StockMove(models.Model):
         is called for a new lines.
         """
         res = super()._action_assign(force_qty=force_qty)
-        for move in self.filtered(
+        for line in self.filtered(
             lambda m: m.state
             in ["confirmed", "assigned", "waiting", "partially_available"]
         ):
-            if move._should_bypass_reservation():
-                continue
-            for move_line in move.move_line_ids.filtered(
+            if line._should_bypass_reservation() or not line._get_auto_fill_flag():
+                return res
+            lines_to_update = line.move_line_ids.filtered(
                 lambda l: l.qty_done != l.reserved_uom_qty
-            ):
-                move_line_vals = {"lot_id": move_line.lot_id}
-                if move._check_auto_fill_conditions(move_line_vals):
-                    move_line.write({"qty_done": move_line.reserved_uom_qty})
+            )
+            for move_line in lines_to_update:
+                if not line._get_avoid_lot_assignment_flag() or not move_line.lot_id:
+                    move_line.qty_done = move_line.reserved_uom_qty
         return res
